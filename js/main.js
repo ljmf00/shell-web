8335e23c598d26527d5d60fba7e0e31fb93d8062
Luís Ferreira <lsferreira169@gmail.com>
2016-07-09 00:29:06 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,61 @@
-    jQuery(function($, undefined) {
-        $('#terminal').terminal(function(command, term) {
-            if (command !== '') {
-                try {
-                    var result = window.eval(command);
-                    if (result !== undefined) {
-                        term.echo(new String(result));
-                    }
-                } catch(e) {
-                    term.error(new String(e));
+$(document).click(function(){
+    $("pre#terminal").focus();
+    $("pre#terminal").click();
+});
+
+$(document).keydown(function(ev){
+    if(ev.which == 116) {
+       window.location.reload();
+    }
+});
+
+jQuery(function($, undefined) {
+    $('#terminal').terminal(function(cmd, term) {
+        var buffered = false;
+        if ((cmd.charAt(cmd.length - 1) == '\\') && !(cmd.charAt(cmd.length - 2) == '\\')) {
+            buffered = true;
+            var buffer = cmd.substr(0, cmd.length - 1);
+            term.push(function(cmd, term) {
+                if ((cmd.charAt(cmd.length - 1) == '\\') && !(cmd.charAt(cmd.length - 2) == '\\')) {
+                    term.echo('');
+                    buffer += cmd.substr(0, cmd.length - 1);
+                } else {
+                    buffer += cmd.substr(0, cmd.length - 1);
+                    cmd = buffer;
+                    SwitchCommands(cmd, term);
+                    buffered = false;
+                    term.pop();
                 }
-            } else {
-               term.echo('');
+                $("html").scrollTop($("pre#terminal.terminal").height());
+            }, {
+                prompt: '> ',
+                name: 'shell-ps2'
+            });
+            cmd = buffer;
+        } else {
+            if ((cmd.charAt(cmd.length - 1) == '\\') && (cmd.charAt(cmd.length - 2) == '\\')) {
+                cmd = cmd.substr(0, cmd.length - 1);
             }
-        }, {
-            greetings: 'Javascript Interpreter',
-            name: 'js_demo',
-            height: 200,
-            prompt: 'js> '
-        });
+        }
+        if(!buffered) SwitchCommands(cmd, term);
+        $("html").scrollTop($("pre#terminal.terminal").height());
+    }, {
+        greetings: '┬  ┌─┐┌─┐┌─┐┬─┐┬─┐┌─┐┬┬─┐┌─┐  ┌─┐┬ ┬┌─┐┬  ┬\n│  └─┐├┤ ├┤ ├┬┘├┬┘├┤ │├┬┘├─┤  └─┐├─┤├┤ │  │\n┴─┘└─┘└  └─┘┴└─┴└─└─┘┴┴└─┴ ┴  └─┘┴ ┴└─┘┴─┘┴─┘\n',
+        name: 'shell',
+        prompt: '$ '
     });
+});
+
+function SwitchCommands(c, t) {
+var cmdobj = $.terminal.parse_command(c);
+    switch (cmdobj.name) {
+        case 'ping':
+            t.echo("pong");	
+            break;
+        case '':
+            t.echo('');
+            break;
+        default:
+            t.error('shell: command not found: ' + c);
+    }
+}
