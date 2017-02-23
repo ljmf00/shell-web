8f986818026e05a1216d35fe11b3ad652ad88917
Luís Ferreira <lsferreira169@gmail.com>
2017-02-23 17:30:19 +0000

    [General] Add initial ASCII Art into some files

--- js/misc/rainbow.js
+++ js/misc/rainbow.js
@@ -1,3 +1,18 @@
+/// "js/misc/rainbow.js" -*- JavaScript -*-
+///  Web-based Shell in Javascript
+///    _________.__           .__  .__             __      __      ___.
+///   /   _____/|  |__   ____ |  | |  |           /  \    /  \ ____\_ |__
+///   \_____  \ |  |  \_/ __ \|  | |  |    ______ \   \/\/   // __ \| __ \  | @repo github.com:ljmf00/shell-web.git
+///   /        \|   Y  \  ___/|  |_|  |__ /_____/  \        /\  ___/| \_\ \ | @author Luís Ferreira
+///  /_______  /|___|  /\___  >____/____/           \__/\  /  \___  >___  / | @license GNU Public License v3
+///          \/      \/     \/                           \/       \/    \/
+///  Copyright (c) 2016 - Luís Ferreira. All right reserved
+///  More information in: https://github.com/ljmf00/ (Github Page)
+
+/// This file is part of the Aurora Framework. This framework is free
+/// software; you can redistribute it and/or modify it under the
+/// terms of the GNU Lesser General Public License, v3.
+
 function rainbowString(text, interval = 1, startColor = 0, isStartCRandom = false, isRandom = false, randomSeed = null) {
     // TODO: randomSeed need to be implemented
 
---
+++ js/modules/cookies.js
@@ -0,0 +1,14 @@
+/// "js/modules/cookies.js" -*- JavaScript -*-
+///  Web-based Shell in Javascript
+///    _________.__           .__  .__             __      __      ___.
+///   /   _____/|  |__   ____ |  | |  |           /  \    /  \ ____\_ |__
+///   \_____  \ |  |  \_/ __ \|  | |  |    ______ \   \/\/   // __ \| __ \  | @repo github.com:ljmf00/shell-web.git
+///   /        \|   Y  \  ___/|  |_|  |__ /_____/  \        /\  ___/| \_\ \ | @author Luís Ferreira
+///  /_______  /|___|  /\___  >____/____/           \__/\  /  \___  >___  / | @license GNU Public License v3
+///          \/      \/     \/                           \/       \/    \/
+///  Copyright (c) 2016 - Luís Ferreira. All right reserved
+///  More information in: https://github.com/ljmf00/ (Github Page)
+
+/// This file is part of the Aurora Framework. This framework is free
+/// software; you can redistribute it and/or modify it under the
+/// terms of the GNU Lesser General Public License, v3.
